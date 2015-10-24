3d80e0f76202a6449226f26bfdb246e5c0515601
dougdworkin <doug@dougdworkin.com>
2015-10-23 18:19:19 -0700

    set up general flow of asking questions, checking answer, showing score, and restarting quiz

--- js/myJS.js
+++ js/myJS.js
@@ -17,12 +17,14 @@ var		progress = 0,
 								"<em>1%</em> - Who turned out the lights?x"
 							],
 				answer: 	3,
-				infoSection:"More infirmation about the brightness goes here" 								
+				infoSection:"More infirmation about the brightness goes here. Lorem" +
+							" ipsum dolor sit amet, consectetur adipisicing elit. Aliquam" +
+							" adipisci, mollitia quas autem doloremque maiores perspiciatis" +
+							" dicta odit fugit numquam iusto, ea veniam, id, cupiditate."
 			},
 
 			//Question two		
-			{ 	question : 	"How much sunlight typically " +
-							" makes it's way to the florest floor",
+			{ 	question : 	"This is question 2", 
 				choices: 	[
 								"<em>99%</em> - It's bright and sunny!",
 								"<em>70%</em> - There is a sofy hazy light.",
@@ -34,8 +36,7 @@ var		progress = 0,
 			},
 
 			//Question three		
-			{ 	question : 	"How much sunlight typically " +
-							" makes it's way to the florest floor",
+			{ 	question : 	"This is question 3", 
 				choices: 	[
 								"<em>99%</em> - It's bright and sunny!",
 								"<em>70%</em> - There is a sofy hazy light.",
@@ -47,8 +48,7 @@ var		progress = 0,
 			},
 
 			//Question four		
-			{ 	question : 	"How much sunlight typically " +
-							" makes it's way to the florest floor",
+			{ 	question : 	"This is question 4", 
 				choices: 	[
 								"<em>99%</em> - It's bright and sunny!",
 								"<em>70%</em> - There is a sofy hazy light.",
@@ -81,14 +81,20 @@ function questionSetUp(question){
 	questionTxt.html(question.question);
 	// loop through choice array and enter input choices and labels 
 	for (var i = 0; i < question.choices.length; i++){
-		answerList.append('<label for"a' + (i+1) + '">' +
-					'<input id="a' + (i+1) + '"'+
-					' type="radio" name="questionOne" required value="' + i + '">' +
-					'<span>' + question.choices[i] +
-					'</label>');	
+		answerList.append('<label for="a' + 
+							(i+1) +
+							'">' +
+							'<input id="a' + 
+							(i+1) +
+							'"'+
+							' type="radio" name="questionOne" required value="' + 
+							i + 
+							'">' +
+							'<span>' + 
+							question.choices[i] +
+							'</label>');	
 	}	
 }
-
 
 
 
@@ -96,25 +102,78 @@ function questionSetUp(question){
 function checkAnswer(){
 	event.preventDefault();
 
-	//log answer
+//log answer
 	answer = $('input[type="radio"]:checked').val();
 	
 	$('div.question.choices').fadeOut();
 	$('div.question.answer').fadeIn();
-	//check if right or wrong and display result
+
+//check if right or wrong and display result
 	if(answer == quizQuestions[progress-1].answer) {
 		$('h3.rightWrong').append('Correct!');	
+		score++;
 	} else {
 		$('h3.rightWrong').append('Nice Try but incorrect');
 	}
+//add the answer details
+	$('p.answerDetails').append(quizQuestions[progress-1].infoSection);
+
+// check if last question
+	if(progress == quizQuestions.length) {
+		$('div#buttonHolder').html('<button class="showScore">Show Final Score</button>');
+		$('button.showScore').on('click', showFinalScore);	
+	}
+}
+
+// deletes question, choices, right/wrong text and answer info	
+function clearAll() {
+	$('.question legend').html('');
+	$('div#choiceArea').html('');
+	$('h3.rightWrong').html('');
+	$('p.answerDetails').html('');
+}
+
+function nextQuestion(){
+// hide answer panel - show question panel
+	$('div.question.answer').fadeOut();
+	$('div.question.choices').fadeIn();
+	clearAll();
+// add next question and answers
+	questionSetUp(quizQuestions[progress]);
+	progress++;	// add one to the progress count
+}
+
+function showFinalScore(){
+	clearAll();
+	$('.questionH2').html('YOUR SCORE');
+	$('div#buttonHolder').html('<button class="retakeQuiz">Retake Quiz</button>');
+	$('button.retakeQuiz').on('click', startOver);
+	$('h3.rightWrong').slideDown().css('text-align', 'center');
+	$('h3.rightWrong').html('You got ' + score + ' out of ' + progress + ' right!');
 
 }
 
+function startOver(){
+	progress = 0;
+	score = 0;
+	clearAll();
+	$('h3.rightWrong').css('text-align', 'left');
+	$('div#buttonHolder').html('<button class="nextQ">Next Question</button>');
+	$('.questionH2').html('Question <span class=qNumber></span>');
+	$('div.question.answer').fadeOut();
+	$('div.question.choices').fadeIn();
+	$('button.nextQ').on('click', nextQuestion);
+	startQuiz();
+}
 
 
-$('div#intro button').on('click', startQuiz);
+$('div#intro button.startQuiz').on('click', startQuiz);
 
 $('div.question.choices input[type="submit"]').on('click', checkAnswer);
+
+$('button.nextQ').on('click', nextQuestion);
+
+
 
 
 
